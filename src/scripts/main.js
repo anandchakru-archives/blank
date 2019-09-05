2615bca61c391938c432d2bf16fc53fc06a9d330
Anand C <anandchakru.forum@gmail.com>
2019-09-05 16:51:11 -0700

    thumbnail cleanup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,3 @@
  * Custom javascript  goes here
  *
  */
-
-
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slider-img-container");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex++;
-  if (myIndex > x.length) { myIndex = 1 }
-  x[myIndex - 1].style.display = "block";
-  setTimeout(carousel, 2000);
-}
--- thumbnails.js
+++ thumbnails.js
@@ -3,35 +3,38 @@ const puppeteer = require('puppeteer');
 // console.log(JSON.stringify(puppeteer.devices));
 const devices = {
   xs: puppeteer.devices['iPhone 5'], sm: puppeteer.devices['iPad'], md: puppeteer.devices['Kindle Fire HDX'],
-  lg: puppeteer.devices['iPad Pro'], xl: puppeteer.devices['Kindle Fire HDX landscape']
+  lg: puppeteer.devices['iPad Pro'], xl: puppeteer.devices['Kindle Fire HDX landscape'], fp: puppeteer.devices['Kindle Fire HDX landscape']
 }
 const deviceIds = Object.keys(devices);
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const takeSs = (browser, id) => {
   return new Promise(async (resolve) => {
-    console.log(`${id} started.`);
+    console.log(`${id} initialized.`);
     const page = await browser.newPage();
     page.once('load', async () => {
-      console.log(`${id} loaded.`);
+      console.log(`${id} page loaded.`);
       await sleep(5000);
-      console.log(`${id} screenshot.`);
-      await page.screenshot({ path: `./src/assets/thumbnail-${id}.jpg` });
-      console.log(`${id} done.`);
+      console.log(`${id} screenshot start.`);
+      await page.screenshot({ path: `./dist/assets/thumbnail-${id}.jpg`, fullPage: id === 'fp' });
+      console.log(`${id} screenshot end.`);
       await sleep(1000);
       await page.close();
       console.log(`${id} closed.`);
     });
     await page.emulate(devices[id]);
     await page.goto('https://nesign.github.io/niview');
-    console.log(`${id} done.`);
+    console.log(`${id} page loading.`);
     return resolve();
   });
 }
 (async () => {
-  //const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
-  const browser = await puppeteer.launch();
+  //const browser = await puppeteer.launch({ headless: false, slowMo: 1250, ignoreHTTPSErrors: true, args: ['--no-sandbox'] });
+  const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, args: ['--no-sandbox'] });
   for (let i = 0; i < deviceIds.length; i++) {
     await takeSs(browser, deviceIds[i]);
     await sleep(6000);
   }
+  console.log('screenshots completed!');
+  await sleep(6000);
+  browser.close();
 })();
